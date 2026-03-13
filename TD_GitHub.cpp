72585d6ee573e8bf66ed73015f52fc5f79c0c1cc
jeniferlaleye47-glitch <geniferlaleye47@gmail.com>
2026-03-13 10:06:06 +0100

    gege

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -21,3 +21,44 @@
 
 
 //Fonctions réalisées par l'étudiant 3 :
+
+bool estmpaire(int nb) 
+{
+    
+    if ((nb % 2 != 0) || (nb % 8 == 0)) 
+	{
+        return true;
+    }
+	else 
+	{
+        return false;
+    }
+}
+
+
+float moyenne(int nb1, int nb2, int nb3) 
+{
+    float somme = nb1 + nb2 + nb3;
+    return somme / 3.0; 
+}
+
+
+float reduction(int prix, int reduc) 
+{
+    
+    float decimalReduc = reduc / 100.0; 
+    float prixApresRemise = prix * (1.0 - decimalReduc);
+    
+    return prixApresRemise * 1.055;
+}
+
+
+int nombreMedian(int a, int b, int c) 
+{
+   
+    if ((a >= b && a <= c) || (a <= b && a >= c)) return a;
+
+    if ((b >= a && b <= c) || (b <= a && b >= c)) return b;
+    
+    return c;
+}
