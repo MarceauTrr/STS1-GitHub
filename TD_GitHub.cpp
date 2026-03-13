d82877dc2a8b6b43a9b629059a467a16abf5352a
MarceauTrr <touriermarceau@gmail.com>
2026-03-13 10:05:42 +0100

    Maj et expo ajouter

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,13 +1,20 @@
 //Fonctions réalisées par l'étudiant 1 :
 
+bool estMajeur(int age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-
-
-
-
-
-
-
+int exposant(int nbre, int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; ++i) {
+        result = result * nbre;
+    }
+    return result;
+}
 
 //Fonctions réalisées par l'étudiant 2 :
 
